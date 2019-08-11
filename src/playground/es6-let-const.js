4c3dc5bf50456d14cf99ebbab6e880fb3fb0a3cf
ccesar <ccesar@MacBook-Pro-de-ccesar.local>
2019-08-11 14:49:39 -0300

    Aside - Playground const & let

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,6 @@
+const fullName = 'Caio Cesar Alves Timoteo';
+let firstName = '';
+let lastName = '';
+
+console.log(fullName ? firstName = fullName.split(' ')[0] : 'Theres no Name');
+console.log(fullName ? firstName = fullName.split(' ')[3] : 'Theres no Name');
